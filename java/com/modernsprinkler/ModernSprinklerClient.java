f2bd8822a3590c2d64a174c3f407f646f7ff8d47
Cryp-Cee <ccspammm@gmail.com>
2025-09-02 12:43:14 +0200

    Add files via upload

--- java/com/modernsprinkler/ModernSprinklerClient.java
+++ java/com/modernsprinkler/ModernSprinklerClient.java
@@ -7,12 +7,19 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import software.bernie.geckolib.GeckoLib;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModernSprinklerClient {
 
     @SubscribeEvent
     public static void onClientSetup(FMLClientSetupEvent event) {
+        // HIER IST DER FEHLENDE BEFEHL:
+        // Wir müssen GeckoLib auf der Client-Seite initialisieren,
+        // BEVOR wir versuchen, seine Renderer zu registrieren.
+        GeckoLib.initialize();
+        
+        // Dieser Teil ist bereits korrekt und registriert unseren Renderer.
         event.enqueueWork(() -> {
             BlockEntityRenderers.register(BlockEntityInit.SPRINKLER_HEAD_BLOCK_ENTITY.get(), SprinklerHeadRenderer::new);
         });
